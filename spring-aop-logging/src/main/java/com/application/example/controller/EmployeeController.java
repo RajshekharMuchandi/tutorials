b320fcea163c59daf762559e163e4934c1571581
RajshekharMuchandi <rsmuchandi@gmail.com>
2020-05-31 16:39:02 +0530

    spring-aop implementation

---
+++ spring-aop-logging/src/main/java/com/application/example/controller/EmployeeController.java
@@ -0,0 +1,51 @@
+package com.application.example.controller;
+
+import com.application.example.exception.ResourceNotFoundException;
+import com.application.example.model.Employee;
+import com.application.example.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity< Employee > getEmployeeById(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        Employee employee = employeeService.getEmployeeById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+        return ResponseEntity.ok().body(employee);
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.createEmployee(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity < Employee > updateEmployee(@PathVariable(value = "id") Long employeeId,
+                                                      @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+        Employee updatedEmployee = employeeService.updateEmployee(employeeId, employeeDetails);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public Map< String, Boolean > deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        return employeeService.deleteEmployee(employeeId);
+    }
+}
+
